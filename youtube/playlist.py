7bf07c4849843e7c5b4c0c2ef079389585f9f4ed
joshuadeanhack <joshuadeanepq@gmail.com>
2023-05-29 19:10:34 +0100

    [Fix] [Youtube] Now creates video objects

--- youtube/playlist.py
+++ youtube/playlist.py
@@ -1,31 +1,52 @@
-import os
-from googleapiclient.discovery import build
+import subprocess
+import json
+import sys
+from dataclasses import dataclass
+from typing import Iterator
 
-# You must set these environment variables to your own values
-os.environ["YOUTUBE_API_SERVICE_NAME"] = "youtube"
-os.environ["YOUTUBE_API_VERSION"] = "v3"
-os.environ["YOUTUBE_API_KEY"] = "YOUR_YOUTUBE_API_KEY"  # Replace with your YouTube API key
 
-playlist_id = "YOUR_PLAYLIST_ID"  # Replace with your playlist ID
+@dataclass
+class Video:
+    index: int
+    title: str
+    id: str
 
-def get_playlist_videos(playlist_id):
-    youtube = build(
-        os.environ["YOUTUBE_API_SERVICE_NAME"],
-        os.environ["YOUTUBE_API_VERSION"],
-        developerKey=os.environ["YOUTUBE_API_KEY"]
-    )
 
-    request = youtube.playlistItems().list(
-        part="snippet",
-        maxResults=25,
-        playlistId=playlist_id
-    )
-    response = request.execute()
+def run_ytdlp_command(playlist_url: str):
+    command = ["yt-dlp", "-j", "--flat-playlist", playlist_url]
+    process = subprocess.run(command, capture_output=True, text=True)
+    return process.stdout
 
-    for item in response['items']:
-        video_title = item['snippet']['title']
-        video_id = item['snippet']['resourceId']['videoId']
-        print(f"Title: {video_title}, ID: {video_id}")
+
+def parse_json_output(output: str) -> Iterator[dict]:
+    lines = output.splitlines()
+    for line in lines:
+        yield json.loads(line)
+
+
+def create_video_objects(video_dicts: Iterator[dict]) -> Iterator[Video]:
+    for i, video_dict in enumerate(video_dicts):
+        yield Video(index=i+1, title=video_dict["title"], id=video_dict["id"])
+
+
+def print_video_info(videos: Iterator[Video]):
+    for video in videos:
+        print(f'{video.index}: {video.title} (ID: {video.id})')
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python playlist.py <playlist_url>")
+        sys.exit(1)
+
+    playlist_url = sys.argv[1]
+
+    ytdlp_output = run_ytdlp_command(playlist_url)
+    video_dicts = parse_json_output(ytdlp_output)
+    videos = create_video_objects(video_dicts)
+
+    print_video_info(videos)
+
 
 if __name__ == "__main__":
-    get_playlist_videos(playlist_id)
+    main()
