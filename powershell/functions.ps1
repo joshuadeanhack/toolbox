eb6bb2ba9458ddb9b0f3605a0c94712f36ba2380
joshuadeanhack <joshuadeanepq@gmail.com>
2023-06-26 19:19:15 +0100

    Update functions.ps1

--- powershell/functions.ps1
+++ powershell/functions.ps1
@@ -21,3 +21,35 @@ function Ensure-PathExists {
     }
 }
 
+function Compress-Folder {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string] $Path,
+        
+        [Parameter(Mandatory=$false)]
+        [string] $DestinationPath
+    )
+
+    if ([string]::IsNullOrEmpty($DestinationPath)) {
+        $DestinationPath = Join-Path (Split-Path -Path $Path -Parent) (Split-Path -Path $Path -Leaf + ".zip")
+    }
+    
+    # Compress the folder
+    Compress-Archive -Path $Path -DestinationPath $DestinationPath -Force
+
+    Write-Output "Folder '$Path' has been compressed to '$DestinationPath'."
+ 
+}
+
+function Compress-FolderExcludeXVC($sourceDir, $destDir, $newName){
+    # Compress-Archive -Path $sourceDir -DestinationPath "$destDir\$newName.zip" -CompressionLevel Optimal -Exclude *.xvc
+    Get-ChildItem -Path $sourceDir -Exclude "*.xvc" | Compress-Archive -DestinationPath $destDir\$newName.zip -CompressionLevel Optimal -Update
+    Write-Host "Compressing: $sourceDir"
+    Write-Host "Destination: $destDir\$newName.zip"
+}
+
+
+# Test-Path -Path "C:\"
+# Ensure-PathExists -Path "C:\"
+# Compress-MyFolder -Path "C:\SomePath\SomeFolder" OR Compress-MyFolder -Path "C:\SomePath\SomeFolder" -DestinationPath "C:\SomePath\CompressedFolder.zip"
+# 
