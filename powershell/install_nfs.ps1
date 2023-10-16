a569707a523812efe9d6b59cdb74d673646c6d92
joshuadeanhack <joshuadeanepq@gmail.com>
2023-10-16 11:10:27 +0100

    Update install_nfs.ps1

--- powershell/install_nfs.ps1
+++ powershell/install_nfs.ps1
@@ -9,9 +9,24 @@ function mountNFS {
         [string] $Username
     )
 
-    cmd.exe mount -o nolock -o casesensitive=yes -u:$Username $SharePath $DrivePath 
+    cmd.exe mount -o nolock -o casesensitive=yes $SharePath $DrivePath   #-u:$Username 
 
 }
+
+function Set-RegistryValue {
+    param (
+        [string]$KeyPath,
+        [string]$ValueName,
+        [string]$ValueData,
+        [string]$ValueKind
+    )
+    $key = Get-Item -LiteralPath $KeyPath -ErrorAction SilentlyContinue
+    if ($null -eq $key) {
+        $key = New-Item -Path $KeyPath -Force
+    }
+    Set-ItemProperty -Path $KeyPath -Name $ValueName -Value $ValueData -Type $ValueKind
+}
+
 
 try {
     
@@ -19,13 +34,32 @@ try {
 
     Enable-WindowsOptionalFeature -FeatureName ServicesForNFS-ClientOnly, ClientForNFS-Infrastructure -Online -NoRestart
 
+    Write-Host "Setting registy keys for UID and GID for anonymous user as the root UID [0]."
+    
+    Set-RegistryValue -KeyPath "HKLM:\SOFTWARE\Microsoft\ClientForNFS\CurrentVersion\Default" -ValueName "AnonymousUid" -ValueData "0" -ValueKind "DWORD"
+    Set-RegistryValue -KeyPath "HKLM:\SOFTWARE\Microsoft\ClientForNFS\CurrentVersion\Default" -ValueName "AnonymousGid" -ValueData "0" -ValueKind "DWORD"
+
     Write-Host "Displaying Mount Status..."
 
     cmd.exe mount
 
+    Write-Host ""
     Write-Host "Attempting to mount"
+    Write-Host ""
 
-    mountNFS -SharePath "\\storage.light.fire\mnt\NVMePool\ShareName" -DrivePath "S:" -Username "nfsuser"
+    mountNFS -SharePath "\\storage.light.fire\mnt\NVMePool\ShareName" -DrivePath "S:"
+    
+    Write-Host ""
+    Write-Host "Displaying Mount Status..."
+    Write-Host ""
+    cmd.exe mount
+
+    # Again for another share with a different user
+
+    # Write-Host "Setting UID and GID for NFS User: otheruser"
+    # Set-RegistryValue -KeyPath "HKLM:\SOFTWARE\Microsoft\ClientForNFS\CurrentVersion\Default" -ValueName "otherUserUid" -ValueData 1001 -ValueKind "DWORD"
+    # Set-RegistryValue -KeyPath "HKLM:\SOFTWARE\Microsoft\ClientForNFS\CurrentVersion\Default" -ValueName "otherUserGid" -ValueData 1001 -ValueKind "DWORD"
+    # mountNFS -SharePath "\\storage.light.fire\mnt\NVMePool\ShareName2" -DrivePath "H:" -Username "otheruser" 
 
 }
 catch {
