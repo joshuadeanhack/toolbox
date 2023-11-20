be6a225e301ef34df69c5286f108355c8b7d3dfc
joshuadeanhack <joshuadeanepq@gmail.com>
2023-11-20 16:51:40 +0000

    Create endpoint.ps1

---
+++ powershell/endpoint.ps1
@@ -0,0 +1,132 @@
+#
+# Windows 10/11 Pro - Generic Upgrade Key: VK7JG-NPHTM-C97JM-9MPGT-3V66T 
+#
+
+function Install-Choco {
+    try{ 
+        choco --version
+    }
+    catch {
+        Write-Host "Choco Not Found, Installing...."
+        Set-ExecutionPolicy Bypass -Scope Process -Force
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    }
+}
+
+function Disable-UAC {
+    New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -PropertyType DWord -Value 0 -Force
+    Write-Host "UAC Disabled"
+}
+
+function Disable-Telemetry {
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowDeviceNameInTelemetry" -Type DWord -Value 0
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
+    
+    Get-Service -Name "DiagTrack" | Stop-Service -NoWait -Force
+    Get-Service -Name "DiagTrack" | set-service -StartupType Disabled
+}
+
+function Install-WSL2 {
+    # Install WSL2
+    wsl --install -d Ubuntu-22.04
+
+    # Set default version 2
+    wsl --set-default-version 2
+}
+
+function Ensure-PathExists {
+    param (
+        [string] $Path
+    )
+
+    if (-not (Test-Path $Path)) {
+        New-Item -ItemType Directory -Path $Path -Force
+        Write-Output "Path '$Path' created."
+    }
+}
+
+function DownloadAndInstall {
+    param (
+        [string]$url,
+        [string]$output
+    )
+
+    # Ensure the output directory exists
+    EnsureDirectoryExists -filePath $output
+
+    # Download the file
+    Write-Output "Downloading from: $url "
+    Write-Output "Storing to: $output"
+
+    $webClient = New-Object System.Net.WebClient
+    $webClient.DownloadFile($url, $output)
+
+    # Run the downloaded .exe file
+    Write-Output "Running Installer - Manual Install - Please Configure"
+    Start-Process -FilePath $output -Wait -Passthru
+}
+
+
+
+# Main
+
+try {
+    Disable-UAC
+    Disable-Telemetry
+    # Set Windows Explorer to Show hidden files
+    Set-RegistryValue -KeyPath "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -ValueName "Hidden" -ValueData "1" -ValueKind "DWORD"
+    # Set Windows Explorer to show known file types
+    Set-RegistryValue -KeyPath "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -ValueName "HideFileExt" -ValueData "0" -ValueKind "DWORD"
+
+}
+catch {
+    Write-Host "Error Configuring Windows: "
+    Write-Host $_
+}
+
+try {
+    Write-Host "Installing Web Browsers"
+    Install-Choco
+    choco feature enable -n allowGlobalConfirmation
+    choco install brave
+    choco install googlechrome
+    choco install bitwarden
+}
+catch {
+    Write-Host "Error Installing Web Browsers: "
+    Write-Host $_
+}
+
+try {
+    Write-Host "Installing Tools"
+    $env:ChocoToolsLocation = "C:\tools"
+    choco install cmder
+    choco install dive --% --ia INSTALLDIR="c:\tools\dive"
+    choco install yt-dlp --% --ia INSTALLDIR="c:\tools\yt-dlp"
+    choco install ventoy --% --ia INSTALLDIR="c:\tools\ventoy"
+    choco install awscli 
+    # Get-iplayer
+    Ensure-PathExists -Path "C:\tools\get-iplayer"
+    DownloadAndInstall -url https://github.com/get-iplayer/get_iplayer_win32/releases/download/3.34.0/get_iplayer-3.34.0-windows-x64-setup.exe -output "C:/tools/get-iplayer/get_iplayer-3.34.0-windows-x64-setup.exe"
+    Install-WSL2
+}
+catch {
+    Write-Host "Error Installing Tools: "
+    Write-Host $_
+}
+
+try {
+    Write-Host "Installing Creative:"
+    choco install blender
+    choco install krita
+}
+catch {
+    Write-Host "Error Installing Creative: "
+    Write-Host $_
+}
+
+sfc /scannow
+dism /Online /Cleanup-Image /RestoreHealth
