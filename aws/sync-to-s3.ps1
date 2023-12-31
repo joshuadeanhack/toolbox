7557c2bbd377dd1c9756bcdd18e37b43ee2968ec
joshuadeanhack <joshuadeanepq@gmail.com>
2023-12-31 19:52:54 +0000

    Update sync-to-s3.ps1

--- aws/sync-to-s3.ps1
+++ aws/sync-to-s3.ps1
@@ -1,13 +1,13 @@
-# Run with powershell options -DRY_RUN $false -LOCAL_PATH "../Folder" etc.
+# Run with powershell options -Upload -LOCAL_PATH "../Folder" etc.
 param (
     [string] $AWS_PROFILE = "laptop",
     [string] $AWS_BUCKET_NAME = "jjemerald-yt",
     [string] $LOCAL_PATH = ".",  # Alternative is Get-Location
     [string] $KEY = "",  # Example use "/Film-TV/Showname"
-    [string] $DRY_RUN = $true
+    [switch] $Upload = $false  #pass this as a flag -Upload to actually upload the videos
 )
 
-if ($DRY_RUN)
+if ($Upload -eq $false)
 {
     aws --profile $AWS_PROFILE s3 sync $LOCAL_PATH s3://$AWS_BUCKET_NAME$KEY --size-only --dryrun
 }
